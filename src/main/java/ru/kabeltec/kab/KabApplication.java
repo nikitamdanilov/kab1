2b323d4b19dcc7e7467166b1d194a1396327d5fa
Nikita Danilov <ndanilov@biarum.com>
2019-12-25 15:31:44 +0300

    Initial commit

---
+++ src/main/java/ru/kabeltec/kab/KabApplication.java
@@ -0,0 +1,13 @@
+package ru.kabeltec.kab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kabeltec/kab/KabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kabeltec.kab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
